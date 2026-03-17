1fbb98cb537bde0a4937c571beffd90202a24b0f
Esteban <pauloguti2018@gmail.com>
2026-03-17 11:56:57 -0500

    add icon

--- src/main/java/com/example/escriturarapida/Main.java
+++ src/main/java/com/example/escriturarapida/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -61,6 +62,7 @@ public class Main extends Application {
                 Objects.requireNonNull(getClass().getResource("/com/example/escriturarapida/styles/styles.css")).toExternalForm()
         );
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/escriturarapida/images/F.png"))));
         primaryStage.setTitle("Escritura Rapida");
         primaryStage.show();
     }
